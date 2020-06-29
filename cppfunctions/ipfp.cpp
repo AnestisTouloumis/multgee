5f289eac9ecbdf48783bd752d785292d185ad75c
AnestisTouloumis <A.Touloumis@brighton.ac.uk>
2020-06-29 21:03:41 +0100

    improved ipfp_cpp

--- cppfunctions/ipfp.cpp
+++ cppfunctions/ipfp.cpp
@@ -1,26 +1,22 @@
 #include <RcppArmadillo.h>
 // [[Rcpp::depends(RcppArmadillo)]]
 // [[Rcpp::export]]
-arma::mat ipfp2(arma::mat initial, arma::vec rowmars, arma::vec colmars, 
-                double maxit, double tol) {
-  arma::mat ans = initial;
-  arma::vec rowsums = vectorise(sum(ans,1));
-  arma::vec colsums = vectorise(sum(ans,0));
-  arma::vec rowweights = rowmars/rowsums;
-  arma::vec colweights = colmars/colsums;
-  double crit = tol + 1;
-  for (int i=0; i < maxit; i++)
-  {
-    rowweights = rowmars/rowsums;
-    ans = diagmat(rowweights) * ans;
-    colsums = vectorise(sum(ans,0));
-    crit = max(abs(colsums - colmars));
-    if(crit<=tol) i=maxit;
-    colweights = colmars/colsums;
-    ans =  ans * diagmat(colweights);
-    rowsums = vectorise(sum(ans,1));
-    crit = max(abs(rowsums - rowmars));
-    if(crit<=tol) i=maxit;
-  }  
+arma::mat ipfp_cpp(arma::mat initial_table,
+                   arma::colvec row_marginals,
+                   arma::rowvec col_marginals,
+                   double maxiter,
+                   double tolerance) {
+  arma::mat ans = initial_table;
+  arma::colvec row_sums = sum(ans, 1);
+  for (int i=0; i<maxiter; i++) {
+    ans.each_col() %= row_marginals/row_sums;
+    arma::rowvec col_sums = sum(ans, 0);
+    double criterion = max(abs(col_sums - col_marginals));
+    if (criterion <= tolerance) break;
+    ans.each_row() %= col_marginals/col_sums;
+    row_sums = sum(ans, 1);
+    criterion = max(abs(row_sums - row_marginals));
+    if (criterion <= tolerance) break;
+    }
   return ans;
 }
